52174634de07afc3fafea693eb96440fcb36a65d
edgar <mloweedgar0@gmail.com>
2022-05-05 12:26:53 +0300

    feat: trigger heroku push

--- src/App/navigation/ProjectHome.js
+++ src/App/navigation/ProjectHome.js
@@ -28,6 +28,7 @@ const routes = [
         description: modules.initiatives,
         disabled: true,
     },
+    
     {
         name: 'Monitoring & Evaluation',
         path: '/monitoring-and-evaluation',
