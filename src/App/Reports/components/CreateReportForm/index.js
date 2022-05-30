fee35d76fd1a06e3cca7293e044e52abdb73ff5f
edgar <mloweedgar0@gmail.com>
2022-05-30 22:50:38 +0300

    feat(reports): add goback button in create report form

--- src/App/Reports/components/CreateReportForm/index.js
+++ src/App/Reports/components/CreateReportForm/index.js
@@ -1,8 +1,21 @@
 
+import { Button } from "antd";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import DisplaySurveyForm from "../../../components/DisplaySurveyForm";
 
 
-const CreateReportForm = () => (<DisplaySurveyForm survey_id="apQUo4bqoEHmKNoPgaPq6F" />);
+const CreateReportForm = (props) => {
+
+    const history = useHistory();
+
+    return (
+    <div>
+        <Button type="primary" onClick={() => history.goBack()} style={{ position: 'absolute', margin: 20 }}>Go Back</Button>
+        <DisplaySurveyForm survey_id="apQUo4bqoEHmKNoPgaPq6F" />
+    </div>
+    );
+}
+
 
 export default CreateReportForm;
