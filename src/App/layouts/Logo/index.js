e44b8d65a6b8774285180bb758399e416a2fb781
edgar <mloweedgar0@gmail.com>
2022-06-13 00:24:26 +0300

    feat: properly handle toggle side menu

--- src/App/layouts/Logo/index.js
+++ src/App/layouts/Logo/index.js
@@ -8,13 +8,13 @@ import './styles.css';
 const Logo = (props) => {
     const { collapsed, setCollapsed } = props;
     return (
-        <Row>
-            <Col span={6}>
+        <Row >
+            <Col span={collapsed ? 24 : 6 } align='center'>
                 <div className="logo-menu" onClick={() => setCollapsed(!collapsed)}>
                     {collapsed ? <MenuUnfoldOutlined className='logo-menu__icon' /> : <MenuFoldOutlined className='logo-menu__icon' />}
                 </div>
             </Col>
-            <Col offset={1} span={17}>
+            <Col offset={0} span={collapsed ? 0 : 17}>
                 <div className="logo__title">ReProST</div>
             </Col>
         </Row>
