591ea025170435695baa58da35a1d5a34b80a5c6
edgar <mloweedgar0@gmail.com>
2022-07-18 17:26:59 +0300

    test: ensure safeguard concerns can be uploaded in the overview page

--- cypress/integration/overview_spec.js
+++ cypress/integration/overview_spec.js
@@ -13,18 +13,27 @@ describe('Overview', () => {
         cy.get('.geonode-layers-control').click('center', { force: true });
     });
 
-    // after(() => {
-    //     cy.SignOut();
-    // });
+    after(() => {
+        cy.SignOut();
+    });
 
     it('should be able to import safeguard concerns', () => {
         cy.intercept({
             method: 'GET',
             url: '/api/v1/procuring_entities/statistics/1'
         }, 
-        { fixture: 'procuring_entities_statistics.json' });
+        { fixture: 'procuring_entities_statistics.json' }).as('procuring_entities_statistics');
+        cy.intercept({
+            method: 'POST',
+            url: '/api/v1/safeguard_concerns/import'
+        }, 
+        { fixture: 'safeguard_concerns_import_respose.json' }).as('importSafeguardConcerns');
+        
         cy.visit('http://localhost:3000/#!/procuring_entity/1/overview');
         cy.contains('Import EHS or Safeguard concerns').should('exist').click();
-        cy.wait(10000);
+        cy.get('#import-ehs-or-safeguard-concerns').selectFile('cypress/fixtures/safeguard_concerns_demo_data.xlsx', { force: true });
+        cy.wait('@importSafeguardConcerns').its('response.statusCode').should('eq', 200);
+
+
     });
 })
